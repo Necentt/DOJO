bad2d1914738cc3a8853882da7b589a2721b4e8b
Leilin <105161004+Necentt@users.noreply.github.com>
2024-04-08 15:33:47 +0300

    refactor

--- test/qwe.py
+++
@@ -1,9 +0,0 @@
-import json
-f = open('../data/json_for_lm/harmful_behaviors_filtered-aim.json')
-data = json.load(f)
-print(data.keys())
-print(data['harmful_behaviors'][0]['instruction_with_behavior'])
-print(data['method_type'])
-
-
-
